f5d00ab7df97608b05f9080be1fb7463cc903b6f
Jacob Ogren <jogren0044@gmail.com>
2019-07-03 15:47:29 -0600

    Clean up code

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-console.log(Math.floor(Math.random() * 50) + 1);
-
 const currentUserRepo = new UserRepository(userData)
 const user = currentUserRepo.findUserData(Math.floor(Math.random() * 50) + 1);
 const userObj = new User({...user})
