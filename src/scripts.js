2da9e9a3f945b051f6f913ac83404e76e5135408
Jacob Ogren <jogren0044@gmail.com>
2019-07-03 15:37:41 -0600

    Populate user info to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -4,3 +4,9 @@ const currentUserRepo = new UserRepository(userData)
 const user = currentUserRepo.findUserData(Math.floor(Math.random() * 50) + 1);
 const userObj = new User({...user})
 console.log(userObj);
+
+$('#header__h2--name').text(userObj.returnFirstName());
+$('#header--address').text(userObj.address);
+$('#header--email').text(userObj.email);
+$('#header--step-goal').text(userObj.dailyStepGoal);
+$('#header--friends').text(userObj.friends);
