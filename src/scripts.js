198505cec1bdeee8ae2528e1c422b2400bc69902
Jacob Ogren <jogren0044@gmail.com>
2019-07-03 15:22:59 -0600

    Console log a random user

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,6 @@
-console.log("Hello World");
+console.log(Math.floor(Math.random() * 50) + 1);
 
-const newUser = new UserRepository(userData, Math.random(1-50)
+const currentUserRepo = new UserRepository(userData)
+const user = currentUserRepo.findUserData(Math.floor(Math.random() * 50) + 1);
+const userObj = new User({...user})
+console.log(userObj);
