35a69b2a4dbaa3f62965e52fdeececf0b293adc6
Jacob Ogren <jogren0044@gmail.com>
2019-07-05 10:58:49 -0600

    Add method to calulate avg for different activity categories for all users

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -10,8 +10,24 @@ class ActivityRepository {
     const newUser = new Activity(targetUserData);
     return newUser.activityData;
   }
+
+  findAvgActivityByDate(date, category) {
+    let filteredDateData = this.data.filter(obj => obj.date === date);
+    let avgForActivity = filteredDateData.reduce((total, current) => {
+      total += current[category];
+      return total
+    }, 0)
+    return Math.round(avgForActivity / filteredDateData.length);
+  }
 }
 
 if (typeof module !== 'undefined') {
   module.exports = ActivityRepository;
 }
+
+
+// For all users, what is the average number of:
+// stairs climbed for a specified date
+// steps taken for a specific date
+// minutes active for a specific date
+// Make a metric of your own! Document it, calculate it, and display it.
--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -21,4 +21,19 @@ describe('ActivityRepository', function() {
     const activityRepo = new ActivityRepository(sampleDataActivity);
     expect(activityRepo.findUserData(7).length).to.equal(3);
   });
+
+  it('should return the average stairs climbed for a specific date', () => {
+    const activityRepo = new ActivityRepository(sampleDataActivity);
+    expect(activityRepo.findAvgActivityByDate('2019/06/15', 'flightsOfStairs')).to.equal(24);
+  })
+
+  it('should return the average steps taken for a specific date', () => {
+      const activityRepo = new ActivityRepository(sampleDataActivity);
+      expect(activityRepo.findAvgActivityByDate('2019/06/15', 'numSteps')).to.equal(7576);
+  })
+
+  it('should return the average minutes active for a specific date', () => {
+      const activityRepo = new ActivityRepository(sampleDataActivity);
+      expect(activityRepo.findAvgActivityByDate('2019/06/15', 'minutesActive')).to.equal(143);
+  })
 })
