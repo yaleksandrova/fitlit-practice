f1b376d24006d61a47ecc07dbd9c1d576ff3a234
Jacob Ogren <jogren0044@gmail.com>
2019-07-05 19:22:40 -0600

    Finish first chart js for hydration

--- src/dataCharts.js
+++ src/dataCharts.js
@@ -1,13 +1,13 @@
-// const Chart = require('../src/Activity');
+console.log(hydration.returnOuncesByWeek('2019/09/22'));
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-  type: 'bar',
+var hydrationChart = document.getElementById('hydrationChart');
+var myChart = new Chart(hydrationChart, {
+  type: 'line',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Today', 'Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday', 'Monday', 'Sunday'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: 'Weekly Water Consumption',
+      data: hydration.returnOuncesByWeek('2019/09/22'),
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -27,13 +27,28 @@ var myChart = new Chart(ctx, {
       borderWidth: 1
       }]
     },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
+      beginAtZero: true
 });
+
+// var dailyStepGoal = document.getElementById('hydrationChart');
+// var stepGoalChart = new Chart(dailyStepGoal, {
+//   type: 'bar',
+//   data: {
+//     labels: ['You', 'The Average'],
+//     datasets: [{
+//       label: 'Step Goal',
+//       data: [user.dailyStepGoal, currentUserRepo.avgStepGoal()],
+//       backgroundColor: [
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(54, 162, 235, 0.2)',
+//       ],
+//       borderColor: [
+//         'rgba(255, 99, 132, 1)',
+//         'rgba(54, 162, 235, 1)',
+//       ],
+//       borderWidth: 1
+//       }]
+//     },
+//       beginAtZero: true
+// });
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,59 @@
 const currentUserRepo = new UserRepository(userData)
-const user = currentUserRepo.findUserData(Math.floor(Math.random() * 50) + 1);
-const userObj = new User({...user})
-console.log(userObj);
+console.log(currentUserRepo)
+// let user;
+let hydration;
+// let sleep;
+// let activity;
+// let activityRepository = new ActivityRepository(activityData)
+// let challenge;
 
-$('#header__h2--name').text(userObj.returnFirstName());
-$('#header--address').text(userObj.address);
-$('#header--email').text(userObj.email);
-$('#header--step-goal').text(userObj.dailyStepGoal);
-$('#header--friends').text(userObj.friends);
+
+function makeRandomUser() {
+  let randomNumber = Math.floor(Math.random() * userData.length)
+  let user = currentUserRepo.findUserData(randomNumber);
+  let userObj = new User({...user})
+  populateUserInfo(userObj);
+  console.log(userObj)
+  getHydrationData(randomNumber);
+  // getSleepData(randomNumber)
+  // getActivityData(randomNumber)
+  // getChallengeData()
+}
+
+makeRandomUser() 
+
+function getHydrationData(randomNumber) {
+  const hydrationRepo = new HydrationRepository(hydrationData);
+  const userHydrationData = hydrationRepo.findUserData(randomNumber);
+  hydration = new Hydration(userHydrationData);
+  console.log(hydration)
+}
+
+function populateUserInfo(userObj) {
+  $('#header__h2--name').text(userObj.returnFirstName());
+  $('#header--address').text(userObj.address);
+  $('#header--email').text(userObj.email);
+  $('#header--step-goal').text(userObj.dailyStepGoal);
+  $('#header--friends').text(userObj.friends);
+}
+
+
+
+// const randomUser = generateRandomUser();
+
+
+// const currentUserRepo = new UserRepository(userData)
+// const user = currentUserRepo.findUserData(randomUser);
+// const userObj = new User({...user})
+// console.log(userObj);
+
+
+
+// const hydrationRepo = new HydrationRepository(hydrationData);
+// const userHydrationData = hydrationRepo.findUserData(randomUser);
+// const hydrationUser = new Hydration(userHydrationData);
+// console.log(hydrationUser)
+
+// function generateRandomUser() {
+//   return Math.floor((Math.random() * 50) + 1);
+//     };
