064752a82c5054b7cf7fda323d8d90f27e897ded
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-06 12:47:34 -0600

    Complete iteration 3  has some errors

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -1,5 +1,5 @@
 // const Sleep = require('../src/Sleep');
-const data = require('../data/sampleDataSleep')
+// const data = require('../data/sampleDataSleep')
 
 class SleepRepository {
   constructor (sleepData) {
@@ -16,9 +16,11 @@ class SleepRepository {
 
   avgSleepQltForEachUser() {
   let obj = {};
-
-    data.forEach(function(user){
-    const arrayforSingleUserInfo = data.filter(function(item){
+  console.log(this.sleepData)
+  this.sleepData.forEach(function(user){
+    let arrayforSingleUserInfo = this.sleepData.filter(function(item){
+    // console.log(this.sleepData)
+    // console.log(arrayforSingleUserInfo)
     return item.userID === user.userID
     })
 
@@ -32,14 +34,65 @@ class SleepRepository {
       return obj;
     }
 
+  usersWithOverThreeSleepQuality(date){
+    var arrayOfObjects = this.sleepData; 
+    let obj = {}; 
+
+    arrayOfObjects.forEach(function(user){
+    const index = arrayOfObjects.findIndex(function(item) {
+      return item.date === date;
+    });
+
+    let data = [...arrayOfObjects];
+    let sleepPastWeek = data.splice(index - 8, index + 1);
+    let arrayforSingleUserInfo = sleepPastWeek.map(function(day){
+      console.log(arrayforSingleUserInfo)
+      return day.sleepQuality;
+    })
+
+    // console.log(`For the past week for this specific date, user ${user.userID} had this quality of sleep: ${arrayforSingleUserInfo}`); 
+    // console.log("---------------")
+
+    let average = (arrayforSingleUserInfo.reduce(function(currentSum, currentValue){
+    currentSum += currentValue;
+      return currentSum;
+    }, 0)) / arrayforSingleUserInfo.length; 
+
+    // console.log("The average sleep quality for user " + user.userID + " is " + average); 
+    // console.log("---------------")
+    obj[user.userID] = average;
+    
+  
+    arrayOfObjects.splice(0, index+1); 
+     //start at position 0 and get rid of 9
+    })
+
+    // console.log(obj); 
+
+    let arr = []; 
+    for(let user in obj){
+      if(obj[user] > 3){
+        arr.push(parseInt(user));
+    }
+    }
+
+    if(arr.length > 1){
+      // console.log(`There are multiple users who averaged a sleep quality greater than 3 for this week: ${arr}`); 
+    } else {
+    // console.log(`The user with an ID of ${arr[0]} has an average greater than 3`); 
+    }
+
+      return arr; 
+  }
 
   usersWithMostSleepHours(specificDate) {
     let sleepObj = {};
 
-      data.forEach(userInfo => {
+      this.sleepData.forEach(userInfo => {
       if (userInfo.date === specificDate) {
         sleepObj[userInfo.userID] = userInfo.hoursSlept;
       }
+      console.log(sleep)
     })
 
     let arr = [];
@@ -57,57 +110,6 @@ class SleepRepository {
       return parseInt(getKeyByVal()); 
   }
 
-  usersWithOverThreeSleepQuality(date){
-    var arrayOfObjects = sampleDataSleep; 
-    let obj = {}; 
-
-    arrayOfObjects.forEach(function(user){
-    const index = arrayOfObjects.findIndex(function(item) {
-      return item.date === date;
-    });
-
-    let data = [...arrayOfObjects];
-    let sleepPastWeek = data.splice(index - 8, index + 1);
-    let arrayforSingleUserInfo = sleepPastWeek.map(function(day){
-      return day.sleepQuality;
-    })
-
-    console.log(`For the past week for this specific date, user ${user.userID} had this quality of sleep: ${arrayforSingleUserInfo}`); 
-    console.log("---------------")
-
-    average = (arrayforSingleUserInfo.reduce(function(currentSum, currentValue){
-    currentSum += currentValue;
-      return currentSum;
-    }, 0)) / arrayforSingleUserInfo.length; 
-
-    console.log("The average sleep quality for user " + user.userID + " is " + average); 
-    console.log("---------------")
-    obj[user.userID] = average;
-    
-  
-    arrayOfObjects.splice(0, index+1); 
-     //start at position 0 and get rid of 9
-    })
-
-    console.log(obj); 
-
-    let arr = []; 
-    for(let user in obj){
-      if(obj[user] > 3){
-        arr.push(parseInt(user));
-    }
-    }
-
-    if(arr.length > 1){
-      console.log(`There are multiple users who averaged a sleep quality greater than 3 for this week: ${arr}`); 
-    } else {
-    console.log(`The user with an ID of ${arr[0]} has an average greater than 3`); 
-    }
-
-      return arr; 
-  }
-
-  // usersWithOverThreeSleepQuality('2019/06/22');
 
 }
 
