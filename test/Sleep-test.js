ad08473c31a70867056fc0c4d9b99765ed47523f
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-09 11:29:45 -0600

    Add complete Iteration 3

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -18,7 +18,7 @@ describe('Sleep', function() {
 
   it('should calculate average number of hours slept per day for a user', function() {
     const sleep = new Sleep([sampleDataSleep[0], sampleDataSleep[1], sampleDataSleep[2]]);
-    expect(sleep.avgNumOfHrsSleptPerDay()).to.equal(6)
+    expect(sleep.avgNumOfHrsSleptPerDay()).to.equal(5)
   });
 
   it('should calculate average sleep quality per day for a user', function() {
@@ -33,25 +33,22 @@ describe('Sleep', function() {
 
   it('should return sleep quality for a user in a specific day', function() {
     const sleep = new Sleep(sampleDataSleep);
-    expect(sleep.returnSleepQualityByDate('2019/06/15')).to.equal(2.2)
+    expect(sleep.returnSleepQualityByDate('2019/06/15')).to.equal(4)
   });
 
   it('should return the index of a specific date', function() {
     const sleep = new Sleep(sampleDataSleep);
-    expect(sleep.findIndexOfSpecificDate('2019/06/17')).to.equal(2)
+    expect(sleep.findIndexOfSpecificDate('2019/06/17')).to.equal(3)
   });
 
   it('should return hours slept on a specific date and 7 days prior', function() {
     const sleep = new Sleep(sampleDataSleep);
-    expect(sleep.returnSleptByWeek('2019/06/22')).to.deep.equal([7, 7.8, 9.3, 10.7, 10.4, 8, 4.1, 6.1]);
+    expect(sleep.returnSleptByWeek('2019/06/22')).to.deep.equal([7, 7.8, 9.3, 10.7, 10.4, 8, 4.1, 6.1, 6.1]);
   });
 
   it('should return slept quality on a specific date and 7 days prior', function() {
     const sleep = new Sleep(sampleDataSleep);
-    expect(sleep.returnSleepQltByWeek('2019/06/22')).to.deep.equal([3, 4.2, 1.2, 1.2, 3.1, 2.6, 3.8, 2.2]);
+    expect(sleep.returnSleepQltByWeek('2019/06/22')).to.deep.equal([3, 4.2, 1.2, 1.2, 3.1, 2.6, 3.8, 4, 1.1]);
   });
-
-  // user 1 =2.5, user 2 = 2.9, user 4 = 3.3
-
 
 });
